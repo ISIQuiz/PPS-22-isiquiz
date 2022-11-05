5b4791757c0c1b3771b85cc8d82efcd9a67f24a2
Daniele Gambaletta <daniele.gambaletta@studio.unibo.it>
2022-11-05 23:33:06 +0100

    Refactored implementation of SavedCourse and SavedQuiz and their tests, added CourseIdentifier

---
+++ src/main/scala/model/CourseIdentifier.scala
@@ -0,0 +1,69 @@
+package model
+
+/**
+ * Trait for the identifier of the course
+ */
+trait CourseIdentifier:
+  /**
+   * @return name of the course
+   */
+  def courseName: String
+
+  /**
+   * @return name of the course degree 
+   */
+  def degreeName: String
+
+  /**
+   * @return name of the course university 
+   */
+  def universityName: String
+
+/**
+ * Object used for managing a course identifier
+ */
+object CourseIdentifier:
+
+  /**
+   * Creates a new [[CourseIdentifier]]
+   * @param courseName the name of the course (E.g. Paradigmi di Programmazione e Sviluppo)
+   * @param degreeName the name of the degree course (E.g. Laurea Magistrale in Ingegneria e Scienze Informatiche)
+   * @param universityName the name of the university (E.g. University of Bologna)
+   * @return CourseIdentifier
+   */
+  def apply(courseName: String, degreeName: String, universityName: String): CourseIdentifier = CourseIdentifierImpl(courseName, degreeName, universityName)
+
+  /**
+   * Case class for the course identifier model
+   * @param courseName the name of the course (E.g. Paradigmi di Programmazione e Sviluppo)
+   * @param degreeName the name of the degree course (E.g. Laurea Magistrale in Ingegneria e Scienze Informatiche)
+   * @param universityName the name of the university (E.g. University of Bologna)
+   */
+  case class CourseIdentifierImpl(override val courseName: String, override val degreeName: String, override val universityName: String) extends CourseIdentifier
+
+  /**
+   * Change the name of the course 
+   * @param courseId the course identifier to edit
+   * @param courseName name of the course
+   * @return CourseIdentifier
+   */
+  def changeCourseName(courseId: CourseIdentifier, courseName: String): CourseIdentifier = courseId match
+    case CourseIdentifierImpl(_, degreeName, universityName) => CourseIdentifier(courseName, degreeName, universityName)
+
+  /**
+   * Change the name of the course  degree
+   * @param courseId the course identifier to edit
+   * @param degreeName name of the course degree
+   * @return CourseIdentifier
+   */
+  def changeDegreeName(courseId: CourseIdentifier, degreeName: String): CourseIdentifier = courseId match
+    case CourseIdentifierImpl(courseName, _, universityName) => CourseIdentifier(courseName, degreeName, universityName)
+
+  /**
+   * Change the name of the course university 
+   * @param courseId the course identifier to edit
+   * @param universityName name of the course university
+   * @return CourseIdentifier
+   */
+  def changeUniversityName(courseId: CourseIdentifier, universityName: String): CourseIdentifier = courseId match
+    case CourseIdentifierImpl(courseName, degreeName, _) => CourseIdentifier(courseName, degreeName, universityName)
--- src/main/scala/model/SavedQuiz.scala
+++ src/main/scala/model/SavedQuiz.scala
@@ -1,12 +1,73 @@
 package model
 
-/* TODO: need to be completed */
+/**
+ *  Trait for a saved quiz
+ */
+trait SavedQuiz extends Quiz:
+  /**
+   * @return a list of answers
+   */
+  def answerList: List[Answer]
 
 /**
- * Case class for saved quiz
- * @param quizText text of the quiz
- * @param maxScore max score if answered correctly
- * @param imagePath image path
+ *  Object used for managing a saved quiz
  */
-case class SavedQuiz(quizText: String, maxScore: Int, imagePath: String) extends Quiz:
-  /*def answer: List[Answer]*/
+object SavedQuiz:
+
+  /**
+   * Creates a new [[SavedQuiz]]
+   * @param text text of the quiz
+   * @param maxScore maximum score when answering a quiz correctly
+   * @param imagePath image path (optional)
+   * @param answerList list of answers
+   * @return SavedQuiz
+   */
+  def apply(text: String, maxScore: Int, imagePath: Option[String], answerList: List[Answer]): SavedQuiz =
+    SavedQuizImpl(text, maxScore, imagePath, answerList)
+
+  /**
+  * Case class for saved quiz model
+  * @param quizText text of the quiz
+  * @param maxScore max score if answered correctly
+  * @param imagePath image path (optional)
+ */
+  case class SavedQuizImpl(override val text: String,
+                           override val maxScore: Int,
+                           override val imagePath: Option[String],
+                           override val answerList: List[Answer]) extends SavedQuiz
+
+  /**
+   * Change text of a saved quiz
+   * @param savedQuiz the saved quiz to edit
+   * @param text a new quiz text
+   * @return SavedQuiz
+   */
+  def changeText(savedQuiz: SavedQuiz, text: String): SavedQuiz = savedQuiz match
+    case SavedQuizImpl(_, maxScore, imagePath, answerList) => SavedQuiz(text, maxScore, imagePath, answerList)
+
+  /**
+   * Change max score of a saved quiz
+   * @param savedQuiz the saved quiz to edit
+   * @param maxScore a new quiz max score
+   * @return SavedQuiz
+   */
+  def changeMaxScore(savedQuiz: SavedQuiz, maxScore: Int): SavedQuiz = savedQuiz match
+    case SavedQuizImpl(text, _, imagePath, answerList) => SavedQuiz(text, maxScore, imagePath, answerList)
+
+  /**
+   * Change image path of a saved quiz
+   * @param savedQuiz the saved quiz to edit
+   * @param imagePath a new quiz image path (optional)
+   * @returna SavedQuiz
+   */
+  def changeImagePath(savedQuiz: SavedQuiz, imagePath: Option[String]): SavedQuiz = savedQuiz match
+    case SavedQuizImpl(text, maxScore, _, answerList) => SavedQuiz(text, maxScore, imagePath, answerList)
+
+  /**
+   * Change the list of answers
+   * @param savedQuiz the saved quiz to edit
+   * @param answerList a new answer list
+   * @return SavedQuiz
+   */
+  def changeAnswerList(savedQuiz: SavedQuiz, answerList: List[Answer]): SavedQuiz = savedQuiz match
+    case SavedQuizImpl(text, maxScore, imagePath, _) => SavedQuiz(text, maxScore, imagePath, answerList)
