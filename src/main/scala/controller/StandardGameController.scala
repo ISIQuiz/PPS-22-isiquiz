0a4d218742364ae436190e583bd3b4abb0a1f8fc
Riccardo Omiccioli <riccardo.omiccioli@studio.unibo.it>
2022-11-09 15:59:51 +0100

    feat(StandardGame): Added basic check on selected answer

--- src/main/scala/controller/StandardGameController.scala
+++ src/main/scala/controller/StandardGameController.scala
@@ -2,6 +2,15 @@ package controller
 
 import controller.Controller.{AppController, PageController}
 import model.GameStage.{GameStage, GameStageImpl}
+import model.Quiz.AnswerList.{getCorrect, getCorrectIndex}
+import model.Quiz.{AnswerList, Quiz}
+import model.QuizInGame
+
+trait GameController:
+  def nextQuiz(): QuizInGame
+  def chooseQuiz(): Quiz
+  def chooseAnswers(): AnswerList
+  def endQuiz(): Unit
 
 /** Companion object of standard game controller */
 object StandardGameController:
@@ -31,4 +40,9 @@ class StandardGameController extends PageController:
   def back: Unit = AppController.handle(AvailablePages.SelectMenu, Option.empty)
 
   def selectAnswer[T](value: Option[T]): Unit =
-    println(value.get)
+    if value.get.toString.toInt -1 == getCorrectIndex(gameStage.quizInGame.answers) then
+      println("Risposta GIUSTA!")
+    else
+      println("Risposta SBAGLIATA!")
+//    println(getCorrectIndex(gameStage.quizInGame.answers))
+//    println(value.get.toString + " | " + getCorrect(gameStage.quizInGame.answers))
