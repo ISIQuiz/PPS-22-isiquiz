bb01a64f48a69a60d8637b78c21e12669c0aa7d0
Riccardo Omiccioli <riccardo.omiccioli@studio.unibo.it>
2022-11-29 22:49:57 +0100

    test(Application): Refactored application to create views with stage and added FX test suite

--- src/main/scala/view/graphicUI/GraphicStandardGameMenu.scala
+++ src/main/scala/view/graphicUI/GraphicStandardGameMenu.scala
@@ -1,25 +1,23 @@
 package view.graphicUI
 
-import controller.AppController.{MainMenuAction, SelectMenuAction}
-import javafx.fxml.FXML
-import javafx.scene.control.Label
-import scalafx.application.JFXApp3.PrimaryStage
-import scalafx.scene.Scene
-import javafx.scene.layout.Pane
-import utils.PaneLoader
+import controller.StandardGameController.Back
 import view.View.{GraphicView, sendEvent}
 import view.updates.ViewUpdate
+import utils.GUILoader
+import utils.GUILoader.loadGUI
+import javafx.fxml.FXML
+import javafx.stage.Stage
 
 object GraphicStandardGameMenu
 
 /** Standard Game menu graphic interface  */
-class GraphicStandardGameMenu(basePanel: Pane) extends GraphicView:
+class GraphicStandardGameMenu(stage: Stage) extends GraphicView:
 
   @FXML
   def selectMenuClicked: Unit =
-    sendEvent(SelectMenuAction)
+    sendEvent(Back)
 
-  PaneLoader.loadPane(basePanel, this, "standard_game.fxml")
+  loadGUI(stage, this, "standard_game.fxml")
 
   override def updateUI[T](update: ViewUpdate[Any]): Unit = {}
 
