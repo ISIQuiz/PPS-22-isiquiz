2cfe83a1a73683b5801b888be6ad69cdaac873ba
Cecilia Teodorani <cecilia.teodorani@studio.unibo.it>
2022-11-30 13:34:59 +0100

    refactor(CustomMenu): refactor of the Graphic Custom Menu

--- src/main/scala/view/graphicUI/GraphicCustomMenu.scala
+++ src/main/scala/view/graphicUI/GraphicCustomMenu.scala
@@ -1,25 +1,22 @@
 package view.graphicUI
 
 import controller.CustomMenuController.Back
-import javafx.fxml.FXML
-import javafx.scene.control.Label
-import javafx.scene.layout.Pane
-import model.GameStage
-import scalafx.application.JFXApp3.PrimaryStage
-import scalafx.scene.Scene
-import utils.PaneLoader
 import view.View.{GraphicView, sendEvent}
 import view.updates.ViewUpdate
+import utils.GUILoader
+import utils.GUILoader.loadGUI
+import javafx.fxml.FXML
+import javafx.stage.Stage
 
 object GraphicCustomMenu
 
-/** Custom settings menu graphic interface  */
-class GraphicCustomMenu(basePanel: Pane) extends GraphicView:
+/** Custom menu graphic interface  */
+class GraphicCustomMenu(stage: Stage) extends GraphicView:
 
   @FXML
   def selectMenuClicked: Unit =
     sendEvent(Back)
 
-  PaneLoader.loadPane(basePanel, this, "custom_menu.fxml")
+  loadGUI(stage, this, "custom_menu.fxml")
 
   override def updateUI[T](update: ViewUpdate[Any]): Unit = {}
