6cc6d1ba0abd7b69218af811c2a94d88360658f4
Cecilia Teodorani <cecilia.teodorani@studio.unibo.it>
2022-12-01 15:32:10 +0100

    feat(SelectMenu): add courses display and selection before starting a game

--- src/main/scala/view/graphicUI/GraphicSelectMenu.scala
+++ src/main/scala/view/graphicUI/GraphicSelectMenu.scala
@@ -1,13 +1,21 @@
 package view.graphicUI
 
-import controller.SelectMenuController.{Back, Custom, Start}
+import controller.SelectMenuController.{Back, Custom, Start, Selection}
 import view.View.{GraphicView, sendEvent}
 import view.updates.ViewUpdate
 import utils.GUILoader
 import utils.GUILoader.loadGUI
 import javafx.fxml.FXML
-import javafx.scene.control.{Button, ScrollPane}
+import javafx.scene.control.{Button, CheckBox, ScrollPane}
+import javafx.scene.layout.VBox
 import javafx.stage.Stage
+import view.SelectMenuView.*
+import javafx.application.Platform
+import javafx.scene.input.MouseEvent
+import model.SavedCourse
+
+import java.util
+import scala.collection.mutable.ListBuffer
 
 object GraphicSelectMenu
 
@@ -30,6 +38,9 @@ class GraphicSelectMenu(stage: Stage) extends GraphicView:
   var customGameButton: Button = _
 
   @FXML
+  var coursesVBox: VBox = _
+
+  @FXML
   def backButtonClicked(): Unit =
     sendEvent(Back)
 
@@ -46,4 +57,26 @@ class GraphicSelectMenu(stage: Stage) extends GraphicView:
 
   loadGUI(stage, this, "select_menu.fxml")
 
-  override def updateUI[T](update: ViewUpdate[Any]): Unit = {}
+  override def updateUI[T](update: ViewUpdate[Any]): Unit = update match
+    case DefaultUpdate => { }
+    case CourseUpdate(updateParameter) =>
+      if update.updateParameter.isDefined then
+        val savedCourses: List[(SavedCourse, Boolean)] = updateParameter.get.asInstanceOf[List[(SavedCourse, Boolean)]]
+        case class CourseToPrint(savedCourse: SavedCourse, isSelected: Boolean)
+        var coursesToPrint: ListBuffer[CourseToPrint] = ListBuffer()
+
+        savedCourses.foreach(course =>
+          coursesToPrint += CourseToPrint(course._1, course._2)
+        )
+        Platform.runLater { () =>
+          coursesVBox.getChildren.clear()
+          coursesToPrint.foreach(courseToPrint =>
+            val checkbox: CheckBox = new CheckBox(s"${courseToPrint.savedCourse.courseId.courseName} (${courseToPrint.savedCourse.quizList.size} quiz)");
+            checkbox.addEventHandler(MouseEvent.MOUSE_PRESSED, event =>
+              sendEvent(Selection(Option(coursesVBox.getChildren.indexOf(checkbox))))
+            );
+            checkbox.setSelected(courseToPrint.isSelected);
+            coursesVBox.getChildren.add(checkbox)
+          )
+
+        }
