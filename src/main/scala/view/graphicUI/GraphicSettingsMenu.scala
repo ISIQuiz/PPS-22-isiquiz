59d382919b4f9a197c236ec93df7911aecc301b0
gambada <daniele.gambaletta@studio.unibo.it>
2022-12-06 01:23:57 +0100

    feat(GraphicSettingsMenu): add import to graphic and alert dialog

--- src/main/scala/view/graphicUI/GraphicSettingsMenu.scala
+++ src/main/scala/view/graphicUI/GraphicSettingsMenu.scala
@@ -7,16 +7,22 @@ import view.updates.ViewUpdate
 import utils.{GUILoader, StorageHandler}
 import utils.GUILoader.loadGUI
 import javafx.fxml.FXML
-import javafx.scene.control.{Button, Label, TextField}
+import javafx.scene.control.Alert.AlertType
+import javafx.scene.control.{Alert, Button, Label, TextField}
 import javafx.stage.FileChooser.ExtensionFilter
 import javafx.stage.{DirectoryChooser, FileChooser, Stage}
-import utils.Configuration.{CurrentDirectoryPath, HomeDirectoryPath}
+import utils.Configuration.{CurrentDirectoryPath, HomeDirectoryPath, PlayerCoursesFileName}
+import utils.StorageHandler.importSavedCourseListFromFile
+
 import java.io.File
+import scala.util.{Failure, Success}
 
 object GraphicSettingsMenu
 
 /** Settings menu graphic interface */
-class GraphicSettingsMenu(stage: Stage) extends GraphicView :
+class GraphicSettingsMenu(stage: Stage) extends GraphicView:
+
+  val alert = Alert(AlertType.NONE)
 
   @FXML
   var backButton: Button = _
@@ -35,16 +41,23 @@ class GraphicSettingsMenu(stage: Stage) extends GraphicView :
 
   @FXML
   var editButton: Button = _
-  
+
   @FXML
   var addCourseButton: Button = _
-  
+
   @FXML
   var addQuizButton: Button = _
 
   @FXML
   def backButtonClicked(): Unit =
     sendEvent(Back)
+
+
+  private def showDialogAlert(alertType: AlertType, title: String, headerText: String, path: String = ""): Unit =
+    alert.setAlertType(alertType)
+    alert.setTitle(title)
+    alert.setHeaderText(headerText + "\n" + path)
+    alert.show()
 
   @FXML
   def importButtonClicked(): Unit =
@@ -54,7 +67,22 @@ class GraphicSettingsMenu(stage: Stage) extends GraphicView :
     fileChooser.setTitle("Importa i corsi")
     fileChooser.setInitialDirectory(File(CurrentDirectoryPath))
     val selectedFile = fileChooser.showOpenDialog(stage)
-    println(selectedFile.getPath)
+    if (selectedFile != null)
+      importSavedCourseListFromFile(AppController.session, selectedFile.getPath) match
+        case Success(savedCourseList) =>
+          AppController.changeSavedCourses(savedCourseList)
+          showDialogAlert(
+            AlertType.INFORMATION,
+            "Importazione",
+            "Importazione avvenuta con successo!",
+            selectedFile.getPath
+          )
+        case Failure(f) =>
+          showDialogAlert(
+            AlertType.ERROR,
+            "Importazione",
+            "Errore! File non valido!"
+          )
 
 
   @FXML
@@ -63,7 +91,22 @@ class GraphicSettingsMenu(stage: Stage) extends GraphicView :
     directoryChooser.setTitle("Esporta i corsi")
     directoryChooser.setInitialDirectory(File(HomeDirectoryPath))
     val selectedDirectory = directoryChooser.showDialog(stage)
-    StorageHandler.exportSavedCoursesToPersonalDirectory(AppController.session, selectedDirectory.toString)
+    if (selectedDirectory != null)
+      StorageHandler.exportSavedCoursesToPath(AppController.session, selectedDirectory.toString) match
+        case Success(path) =>
+          showDialogAlert(
+            AlertType.INFORMATION,
+            "Esportazione",
+            "Esportazione avvenuta con successo!",
+            path
+          )
+        case Failure(f) =>
+          showDialogAlert(
+            AlertType.ERROR,
+            "Esportazione",
+            "Errore! File non valido!"
+          )
+
 
   @FXML
   def editButtonClicked(): Unit = ???
