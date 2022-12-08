35a796619d7c28f11707c43f501d910b7a47f914
Cecilia Teodorani <cecilia.teodorani@studio.unibo.it>
2022-12-08 14:21:08 +0100

    test(Quiz): modify and add tests for Quiz

--- src/test/scala/model/QuizTest.scala
+++ src/test/scala/model/QuizTest.scala
@@ -2,72 +2,64 @@ package model
 
 import org.scalatest.funsuite.AnyFunSuite
 import model.Answer.*
+import Quiz.*
+import List.*
 
-import java.util.UUID
+/** Tests for the [[Quiz]] object */
 class QuizTest extends AnyFunSuite :
 
-  val a1 = Answer("answer", true)
-  val a2 = Answer("answer", false)
-  val a3 = Answer("answer", false)
+  val a1 = Answer("answer1", true)
+  val a2 = Answer("answer2", false)
+  val a3 = Answer("answer3", false)
+  val answerList = List(a1, a2, a3)
+  val quiz: Quiz = Quiz(question = "question", answerList = answerList, maxScore = 10)
 
-  test("Creation Answer") {
-    Answer("answer", true)
-    Answer("answer", false)
-    assert(true)
+  test("Creation Quiz") {
+    assert(quiz != Quiz(question = "question", answerList = List(a1, a2, a3), maxScore = 10))
   }
 
-  test("Get/Change Text Answer") {
-    assert(getText(a1) == "answer")
-    val b = changeText(a1, "new-answer")
-    assert(getText(b) == "new-answer")
+  test("Change max score"){
+    val quiz1 = changeMaxScore(quiz, 50)
+    assert(quiz1.maxScore == 50)
+
+    val quiz2 = changeMaxScore(quiz1, 30)
+    assert(quiz2.maxScore == 30)
   }
 
-  test("isCorrect Answer") {
-    assert(isCorrect(a1))
-    val b = Answer("answer", false)
-    assert(!isCorrect(b))
+  test("Change image path") {
+    assert(!quiz.imagePath.isDefined && quiz.imagePath.isEmpty)
+
+    val quiz1 = changeImagePath(quiz, Option("resources/img.png"))
+    assert(quiz1.imagePath == Option("resources/img.png"))
+    assert(quiz1.imagePath.isDefined && quiz1.imagePath.get == "resources/img.png")
   }
 
-  test("Change Right Answer") {
-    assert(isCorrect(a1))
-    val b = changeCorrect(false)(a1)
-    assert(!isCorrect(b))
-    assert(isCorrect(changeCorrect(true)(Answer("answer", false))))
+  test("Change question") {
+    val quiz1 = changeQuestion(quiz, "New Quiz")
+    assert(quiz1.question == "New Quiz")
 
-    val corrAns = makeCorrect(a1)
-    assert(isCorrect(corrAns))
-    val d = makeWrong(corrAns)
-    assert(!isCorrect(d))
+    val quiz2 = changeQuestion(quiz1, "QUESTION")
+    assert(quiz2.question == "QUESTION")
   }
 
-  test("AnswerList") {
-    import model.Quiz.*
-    val answerList: List[Answer] = List(a1, a2, a3)
+  test("Change answer list"){
+    val a4 = Answer("answer4", true)
+    val answerList1 = answerList ::: List(a4)
+    val quiz1 = changeAnswerList(quiz, answerList1)
 
     assert(answerList.size == 3)
-
-    val a4 = Answer("answer", true)
-    val answerList2 = answerList ::: List(a4)
-
-    assert(answerList2.size == 4)
-
-    assert(answerList.filter(answer => answer.isCorrect) == List(a1))
-    assert(answerList2.filter(answer => answer.isCorrect) == List(a4, a1))
-
-    println(answerList2.toString())
+    assert(answerList1.size == 4 && quiz1.answerList.size == 4)
+    assert(quiz1.answerList.contains(a4))
   }
 
-  test("Quiz") {
-    import Quiz.*
-    val answerList: List[Answer] = List(a1, a2, a3)
-
-    val quiz: Quiz = Quiz(question = "question", answerList = answerList, maxScore = 10)
+  test("Get correct answers"){
     assert(getCorrectAnswers(quiz) == List(a1))
 
-    println(quiz)
-    println(printQuiz(quiz))
-    println(printQuizFull(quiz))
-    println(quiz.answerList.toString)
+    val a4 = Answer("answer4", true)
+    val answerList1 = answerList ::: List(a4)
+    val quiz1 = changeAnswerList(quiz, answerList1)
+
+    assert(getCorrectAnswers(quiz1) == List(a1, a4))
   }
 
 end QuizTest
