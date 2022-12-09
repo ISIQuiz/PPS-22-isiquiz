75cc1be138f96a8e3c8f0d2d0371425bb40f6364
Cecilia Teodorani <cecilia.teodorani@studio.unibo.it>
2022-12-09 17:00:11 +0100

    test(SavedCourse): modify and add tests for SavedCourse

--- src/test/scala/model/SavedCourseTest.scala
+++ src/test/scala/model/SavedCourseTest.scala
@@ -24,70 +24,70 @@ class SavedCourseTest extends AnyFunSuite with Matchers:
   /* Simple saved course */
   val savedCourse: SavedCourse = SavedCourse(courseId, Some("descrizione"), quizList)
 
-  println(savedCourse.toString)
-
-  test("The name should be equal to Paradigmi di Programmazione e Sviluppo") {
-    savedCourse.courseId.courseName shouldEqual "Paradigmi di Programmazione e Sviluppo"
+  test("Get course identifier") {
+    savedCourse.courseId shouldEqual courseId
   }
 
-  test("The course degree name should be equal to Laurea Magistrale in Ingegneria e Scienze Informatiche") {
-    savedCourse.courseId.degreeName shouldEqual "Laurea Magistrale in Ingegneria e Scienze Informatiche"
+  test("Get course description") {
+    assert(savedCourse.description.isDefined)
+    savedCourse.description shouldEqual Some("descrizione")
+    savedCourse.description.get shouldEqual "descrizione"
   }
 
-  test("The university name should be equal to Università di Bologna") {
-    savedCourse.courseId.universityName shouldEqual "Università di Bologna"
-  }
-
-  test("The quiz list should be equal to the starting list") {
+  test("Get quiz list") {
     savedCourse.quizList shouldEqual quizList
   }
 
-  test("Testing change Course Id") {
-    val courseIdChanged = CourseIdentifier("Course", "Degree", "University")
-    val savedCourseChanged = SavedCourse.changeCourseIdentifier(savedCourse, courseIdChanged)
-
-    savedCourseChanged.courseId.courseName shouldEqual "Course"
-    savedCourseChanged.courseId.degreeName shouldEqual "Degree"
-    savedCourseChanged.courseId.universityName shouldEqual "University"
+  test("Get course name") {
+    savedCourse.courseId.courseName shouldEqual "Paradigmi di Programmazione e Sviluppo"
   }
 
-  test("Testing change id CourseName") {
+  test("Get degree name") {
+    savedCourse.courseId.degreeName shouldEqual "Laurea Magistrale in Ingegneria e Scienze Informatiche"
+  }
+
+  test("Get university name") {
+    savedCourse.courseId.universityName shouldEqual "Università di Bologna"
+  }
+
+  test("Change course identifier") {
+    val courseIdChanged = CourseIdentifier("PCD", "Laurea Triennale", "UniBo")
+    val savedCourseChanged = SavedCourse.changeCourseIdentifier(savedCourse, courseIdChanged)
+
+    savedCourseChanged.courseId.courseName shouldEqual "PCD"
+    savedCourseChanged.courseId.degreeName shouldEqual "Laurea Triennale"
+    savedCourseChanged.courseId.universityName shouldEqual "UniBo"
+  }
+
+  test("Change description") {
+    val savedCourseChanged = SavedCourse.changeDescription(savedCourse, Some("Corso per lo studio di..."))
+    savedCourseChanged.description shouldEqual Some("Corso per lo studio di...")
+  }
+
+  test("Change quiz list") {
+    val quizListChanged: List[Quiz] = List(q1, q2)
+
+    val savedCourseChanged = SavedCourse.changeQuizList(savedCourse, quizListChanged)
+    savedCourseChanged.quizList.size shouldEqual 2
+  }
+
+  test("Change id Course Name") {
     val courseIdChanged = CourseIdentifier.changeCourseName(savedCourse.courseId, "Sistemi Operativi")
     val savedCourseChanged = SavedCourse.changeCourseIdentifier(savedCourse, courseIdChanged)
 
     savedCourseChanged.courseId.courseName shouldEqual "Sistemi Operativi"
   }
 
-  test("Testing change id DegreeName") {
+  test("Change id Degree Name") {
     val courseIdChanged = CourseIdentifier.changeDegreeName(savedCourse.courseId, "Laurea in Ingegneria Informatica")
     val savedCourseChanged = SavedCourse.changeCourseIdentifier(savedCourse, courseIdChanged)
 
     savedCourseChanged.courseId.degreeName shouldEqual "Laurea in Ingegneria Informatica"
   }
 
-  test("Testing change id UniversityName") {
+  test("Change id University Name") {
     val courseIdChanged = CourseIdentifier.changeUniversityName(savedCourse.courseId, "Università di Milano")
     val savedCourseChanged = SavedCourse.changeCourseIdentifier(savedCourse, courseIdChanged)
 
     savedCourseChanged.courseId.universityName shouldEqual "Università di Milano"
   }
-
-  test("Testing change description") {
-    val savedCourseChanged = SavedCourse.changeDescription(savedCourse, Some("Corso per lo studio di..."))
-    savedCourseChanged.description shouldEqual Some("Corso per lo studio di...")
-  }
-
-  test("Testing change quiz list") {
-    val quizListChanged: List[Quiz] = List(q1, q2)
-
-    val savedCourseChanged = SavedCourse.changeQuizList(savedCourse, quizListChanged)
-    savedCourseChanged.quizList.size shouldEqual 2
-    println(savedCourseChanged.quizList.toString())
-  }
-
-
-
-
-
-
-
