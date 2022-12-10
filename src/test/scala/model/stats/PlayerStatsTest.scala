c58db78056c8686ab51576a1cf292fc578efbb29
gambada <daniele.gambaletta@studio.unibo.it>
2022-12-10 22:58:37 +0100

    test(PlayerStatsTest): test remove unused stats

--- src/test/scala/model/stats/PlayerStatsTest.scala
+++ src/test/scala/model/stats/PlayerStatsTest.scala
@@ -1,8 +1,9 @@
 package model.stats
 
-import model.{Course, CourseIdentifier}
+import model.Quiz.Quiz
+import model.{Course, CourseIdentifier, SavedCourse}
 import model.stats.CourseInStats.CourseInStats
-import model.stats.PlayerStats.initStats
+import model.stats.PlayerStats.{initStats, removeUnusedStats, updatePlayerStats}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
@@ -11,6 +12,8 @@ import java.util.UUID
 class PlayerStatsTest extends AnyFunSuite with Matchers:
 
   val playerStats = PlayerStats()
+  val ci = CourseIdentifier("courseName", "degreeName", "universityName")
+
 
   test("Test default PlayerStats") {
     playerStats shouldEqual initStats
@@ -43,34 +46,39 @@ class PlayerStatsTest extends AnyFunSuite with Matchers:
   }
 
   test("Test change course in stats list") {
-    val courseInStatsList: List[CourseInStats] = List(
-      CourseInStats(
-        Course(
-          CourseIdentifier("courseName", "degreeName", "universityName")
-        ),
-        List()
-      )
-    )
+    val courseInStatsList: List[CourseInStats] = List(CourseInStats(Course(ci), List()))
     val playerStatsChanged = PlayerStats.changeCourseInStatsList(playerStats, courseInStatsList)
     playerStatsChanged.courseInStatsList shouldEqual courseInStatsList
   }
 
+  val quiz1 = QuizInStats(UUID.fromString("00000000-0000-0000-0000-000000000002"), totalSeen = 4, totalScore = 2, totalRightAnswers = 2, averageTimeAnswer = 2)
+  val quiz2 = QuizInStats(UUID.fromString("00000000-0000-0000-0000-000000000002"), totalSeen = 4, totalScore = 4, totalRightAnswers = 4, averageTimeAnswer = 4)
+
 
   test("Test derived stats and merge") {
-
-    val ci = CourseIdentifier("aaa", "bbb", "ccc")
-    val quiz1 = QuizInStats(UUID.fromString("00000000-0000-0000-0000-000000000002"), totalSeen = 4, totalScore = 2, totalRightAnswers = 2, averageTimeAnswer = 2)
-    val quiz2 = QuizInStats(UUID.fromString("00000000-0000-0000-0000-000000000002"), totalSeen = 4, totalScore = 4, totalRightAnswers = 4, averageTimeAnswer = 4)
-
     val playerStats = PlayerStats(0, 0, 0, 0, 0, List(CourseInStats(Course(ci), List(quiz1))))
-
     val newPlayerStats = PlayerStats(0, 0, 0, 0, 0, List(CourseInStats(Course(ci), List(quiz2))))
-
     val mergedList = PlayerStats.mergePlayerStats(playerStats, newPlayerStats)
     println(mergedList)
     val resultWanted = PlayerStats(6, 8, 6, 75, 3.0, List(CourseInStats(Course(ci), List(QuizInStats(UUID.fromString("00000000-0000-0000-0000-000000000002"), 8, 6, 6, 3.0)))))
     assert(mergedList.equals(resultWanted))
+  }
 
+
+  val quiz3 = QuizInStats(UUID.fromString("00000000-0000-0000-0000-000000000003"), totalSeen = 6, totalScore = 1, totalRightAnswers = 2, averageTimeAnswer = 5)
+
+
+  test("Test remove unused stats") {
+
+    val savedCourseList = List(
+      SavedCourse(ci, Option("description"), List(Quiz(UUID.fromString("00000000-0000-0000-0000-000000000003"), "", List(), 12))))
+
+    val playerStats = PlayerStats(5, 4, 1, 2, 3, List(CourseInStats(Course(ci), List(quiz1, quiz3))))
+    val newPlayerStats = removeUnusedStats(savedCourseList, playerStats)
+    println(newPlayerStats)
+    val wantedResult = PlayerStats(1, 6, 2, 33, 5, List(CourseInStats(Course(ci), List(quiz3))))
+
+    newPlayerStats shouldEqual wantedResult
   }
 
 
