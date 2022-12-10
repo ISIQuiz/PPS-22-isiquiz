9a6f2dc46cb5e2bb9d8e119a8db54206d979e6c7
gambada <daniele.gambaletta@studio.unibo.it>
2022-12-10 18:08:45 +0100

    feat(GraphicReviewMenu): add score and time to answer

--- src/main/scala/view/graphicUI/GraphicReviewMenu.scala
+++ src/main/scala/view/graphicUI/GraphicReviewMenu.scala
@@ -17,7 +17,7 @@ import scalafx.scene.layout.Pane
 
 object GraphicReviewMenu
 
-/** Review menu graphic interface  */
+/** Review menu graphic interface */
 class GraphicReviewMenu(stage: Stage) extends GraphicView:
 
   var showAllAnswers = false
@@ -56,30 +56,33 @@ class GraphicReviewMenu(stage: Stage) extends GraphicView:
       Platform.runLater(() => {
         totPointsLabel.textProperty().set(s"Punti totali: ${updateParameter.get}")
       })
-    case CurrentReviewUpdate(updateParameter) => if updateParameter.isDefined then fillAnswerBox(updateParameter.get.quizAnsweredList)
+    case CurrentReviewUpdate(updateParameter) => if updateParameter.isDefined then
+      fillAnswerBox(updateParameter.get.quizAnsweredList)
     case _ => {}
 
-    def fillAnswerBox(quizAnsweredList: List[QuizAnswered]): Unit =
-      Platform.runLater(() => {
-        quizAnsweredVBox.getChildren.clear()
+  def fillAnswerBox(quizAnsweredList: List[QuizAnswered]): Unit =
+    Platform.runLater(() => {
+      quizAnsweredVBox.getChildren.clear()
+      for
+        quizAnswered <- quizAnsweredList
+      do
+        val quizVBox = VBox()
+        quizVBox.getStyleClass.setAll("review-quiz-box")
+        val quizQuestionLabel = Label(quizAnswered.quizInGame.quiz.question)
+        quizQuestionLabel.getStyleClass.setAll("review-question")
+        quizVBox.getChildren.addAll(quizQuestionLabel)
+        val quizDescLabel = Label(s"(Punti: ${quizAnswered.score}/${quizAnswered.quizInGame.quiz.maxScore}) " +
+          s"- (Tempo: ${quizAnswered.timeToAnswer}) " +
+          s"- ${quizAnswered.quizInGame.course.courseId.courseName}");
+        quizDescLabel.getStyleClass.setAll("label-dark")
+        quizVBox.getChildren.addAll(quizDescLabel)
         for
-          quizAnswered <- quizAnsweredList
+          answerQuiz <- quizAnswered.quizInGame.answers;
+          if (showAllAnswers || answerQuiz.isCorrect || (if quizAnswered.answerPlayer.isDefined then quizAnswered.answerPlayer.get == answerQuiz else false))
         do
-          val quizVBox = VBox()
-          quizVBox.getStyleClass.setAll("review-quiz-box")
-          val quizQuestionLabel = Label(quizAnswered.quizInGame.quiz.question)
-          quizQuestionLabel.getStyleClass.setAll("review-question")
-          quizVBox.getChildren.addAll(quizQuestionLabel)
-          val quizDescLabel = Label(s"(Punti: ${quizAnswered.quizInGame.quiz.maxScore}) - ${quizAnswered.quizInGame.course.courseId.courseName}")
-          quizDescLabel.getStyleClass.setAll("label-dark")
-          quizVBox.getChildren.addAll(quizDescLabel)
-          for
-            answerQuiz <- quizAnswered.quizInGame.answers;
-            if (showAllAnswers || answerQuiz.isCorrect || (if quizAnswered.answerPlayer.isDefined then quizAnswered.answerPlayer.get==answerQuiz else false))
-          do
-            val quizAnswerLabel = Label(s"${answerQuiz.text}")
-            if answerQuiz.isCorrect then quizAnswerLabel.getStyleClass.setAll("review-correct-answer") else quizAnswerLabel.getStyleClass.setAll("review-wrong-answer")
-            quizVBox.getChildren.addAll(quizAnswerLabel);
-          quizAnsweredVBox.setSpacing(10);
-          quizAnsweredVBox.getChildren.addAll(quizVBox);
-      })
+          val quizAnswerLabel = Label(s"${answerQuiz.text}")
+          if answerQuiz.isCorrect then quizAnswerLabel.getStyleClass.setAll("review-correct-answer") else quizAnswerLabel.getStyleClass.setAll("review-wrong-answer")
+          quizVBox.getChildren.addAll(quizAnswerLabel);
+        quizAnsweredVBox.setSpacing(10);
+        quizAnsweredVBox.getChildren.addAll(quizVBox);
+    })
